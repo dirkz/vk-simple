900556473ba29ced5a80fcfd68b024b4b3eb40d5
Dirk Zimmermann <me@dirkz.com>
2025-08-13 14:11:20 +0200

    Create a window, then quit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int SDL_main(int argc, char* argv[])
     SDL_Log("Hello, world!");
 
     sdl::Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = sdl::CreateWindow("vk-simple", 800, 600, SDL_WINDOW_VULKAN);
+
     sdl::Quit();
 
     return 0;
