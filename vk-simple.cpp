2ce097a1a9967f152adb0ff6c3b2adfd0cdf57c3
Dirk Zimmermann <me@dirkz.com>
2025-08-13 13:29:06 +0200

    Add project files.

---
+++ vk-simple.cpp
@@ -0,0 +1,12 @@
+﻿// vk-simple.cpp : Defines the entry point for the application.
+//
+
+#include "vk-simple.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
