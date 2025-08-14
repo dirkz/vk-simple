25f8fb34f6661dc9e90d6988f697692b55356cd0
Dirk Zimmermann <me@dirkz.com>
2025-08-14 14:58:32 +0200

    SwapchainSupportDetails::ChooseSurfaceFormat

--- engine/SwapchainSupportDetails.cpp
+++ engine/SwapchainSupportDetails.cpp
@@ -10,9 +10,24 @@ SwapchainSupportDetails::SwapchainSupportDetails(vk::raii::PhysicalDevice &devic
     m_formats = device.getSurfaceFormatsKHR(surface);
     m_presentModes = device.getSurfacePresentModesKHR(surface);
 }
-bool SwapchainSupportDetails::IsAdequate()
+
+bool SwapchainSupportDetails::IsAdequate()
 {
     return !m_formats.empty() && !m_presentModes.empty();
+}
+
+vk::SurfaceFormatKHR SwapchainSupportDetails::ChooseSurfaceFormat()
+{
+    for (const auto &format : m_formats)
+    {
+        if (format.format == vk::Format::eB8G8R8A8Srgb &&
+            format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear)
+        {
+            return format;
+        }
+    }
+
+    return m_formats[0];
 }
 
 } // namespace vksimple
--- engine/SwapchainSupportDetails.h
+++ engine/SwapchainSupportDetails.h
@@ -10,6 +10,7 @@ struct SwapchainSupportDetails
     SwapchainSupportDetails(vk::raii::PhysicalDevice &device, vk::raii::SurfaceKHR &surface);
 
     bool IsAdequate();
+    vk::SurfaceFormatKHR ChooseSurfaceFormat();
 
   private:
     vk::SurfaceCapabilitiesKHR m_capabilities;
