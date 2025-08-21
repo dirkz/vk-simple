696212f8502effcfc3dab7c79b4acace5e447b04
Dirk Zimmermann <me@dirkz.com>
2025-08-21 10:29:18 +0200

    Basic FrameData implementation

--- engine/FrameData.cpp
+++ engine/FrameData.cpp
@@ -3,4 +3,20 @@
 namespace vksimple
 {
 
+FrameData::FrameData(vk::raii::Device &device, vk::raii::CommandPool &commandPool)
+{
+    constexpr uint32_t commandBufferCount = 1;
+    vk::CommandBufferAllocateInfo commandBufferAllocateInfo{
+        commandPool, vk::CommandBufferLevel::ePrimary, commandBufferCount};
+
+    auto commandBuffers = device.allocateCommandBuffers(commandBufferAllocateInfo);
+    m_commandBuffer = std::move(commandBuffers[0]);
+
+    vk::SemaphoreCreateInfo semaphoreCreateInfo{};
+    m_imageAvailableSemaphore = device.createSemaphore(semaphoreCreateInfo);
+
+    vk::FenceCreateInfo fenceCreateInfo{vk::FenceCreateFlagBits::eSignaled};
+    m_inflightFence = device.createFence(fenceCreateInfo);
 }
+
+} // namespace vksimple
--- engine/FrameData.h
+++ engine/FrameData.h
@@ -7,6 +7,27 @@ namespace vksimple
 
 struct FrameData
 {
+    FrameData(vk::raii::Device &device, vk::raii::CommandPool &commandPool);
+
+    vk::raii::CommandBuffer &CommandBuffer()
+    {
+        return m_commandBuffer;
+    }
+
+    vk::raii::Semaphore &ImageAvailableSemaphore()
+    {
+        return m_imageAvailableSemaphore;
+    }
+
+    vk::raii::Fence &InflightFence()
+    {
+        return m_inflightFence;
+    }
+
+  private:
+    vk::raii::CommandBuffer m_commandBuffer = nullptr;
+    vk::raii::Semaphore m_imageAvailableSemaphore = nullptr;
+    vk::raii::Fence m_inflightFence = nullptr;
 };
 
 } // namespace vksimple
