8dcc4d0fe5d2a0db670bc577a1fe1af683b0f976
Dirk Zimmermann <me@dirkz.com>
2025-08-24 18:13:23 +0200

    Less verbose memory search

--- engine/Buffer.cpp
+++ engine/Buffer.cpp
@@ -10,11 +10,9 @@ uint32_t Buffer::FindMemoryType(vk::raii::PhysicalDevice &physicalDevice, uint32
 
     for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; ++i)
     {
-        vk::MemoryType memoryType = memoryProperties.memoryTypes[i];
-        vk::MemoryPropertyFlags propertyFlags = memoryType.propertyFlags;
-        uint32_t iShifted = 1 << i;
-        bool memoryTypeBitsMatch = memoryTypeBits & iShifted;
-        vk::MemoryPropertyFlags propertyFlagsAnded = propertyFlags & memoryPropertyFlags;
+        bool memoryTypeBitsMatch = memoryTypeBits & (1 << i);
+        vk::MemoryPropertyFlags propertyFlagsAnded =
+            memoryProperties.memoryTypes[i].propertyFlags & memoryPropertyFlags;
         if (memoryTypeBitsMatch && propertyFlagsAnded == memoryPropertyFlags)
         {
             return i;
