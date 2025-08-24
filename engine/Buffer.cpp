451b441da9433bd75ac6b6f130982ee413d3cf68
Dirk Zimmermann <me@dirkz.com>
2025-08-24 15:51:45 +0200

    FindMemoryType

--- engine/Buffer.cpp
+++ engine/Buffer.cpp
@@ -3,4 +3,21 @@
 namespace vksimple
 {
 
+uint32_t Buffer::FindMemoryType(vk::PhysicalDevice &physicalDevice, uint32_t memoryTypeBits,
+                                vk::MemoryPropertyFlags memoryPropertyFlags)
+{
+    vk::PhysicalDeviceMemoryProperties memoryProperties = physicalDevice.getMemoryProperties();
+
+    for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; ++i)
+    {
+        if ((memoryTypeBits & (1 << i)) &&
+            (memoryProperties.memoryTypes[i].propertyFlags & memoryPropertyFlags))
+        {
+            return i;
+        }
+    }
+
+    throw std::runtime_error{"no suitable memory found"};
 }
+
+} // namespace vksimple
--- engine/Buffer.h
+++ engine/Buffer.h
@@ -7,6 +7,8 @@ namespace vksimple
 
 struct Buffer
 {
+    static uint32_t FindMemoryType(vk::PhysicalDevice &physicalDevice, uint32_t memoryTypeBits,
+                                   vk::MemoryPropertyFlags memoryPropertyFlags);
 };
 
 } // namespace vksimple
