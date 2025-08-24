6b9038e53198d045abdcddec5b13d45acb617717
Dirk Zimmermann <me@dirkz.com>
2025-08-24 09:08:58 +0200

    Fix the vertex input confusion between location and binding

--- engine/Vertex.h
+++ engine/Vertex.h
@@ -19,9 +19,9 @@ struct Vertex
         constexpr uint32_t binding = 0;
         constexpr uint32_t location = 0;
         vk::VertexInputAttributeDescription description1{
-            binding, location, vk::Format::eR32G32Sfloat, offsetof(Vertex, m_position)};
+            location, binding, vk::Format::eR32G32Sfloat, offsetof(Vertex, m_position)};
         vk::VertexInputAttributeDescription description2{
-            binding, location + 1, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, m_color)};
+            location + 1, binding, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, m_color)};
 
         return {description1, description2};
     }
