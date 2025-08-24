1d5dc5f0888de921f3357241fda8f7264a45eb5e
Dirk Zimmermann <me@dirkz.com>
2025-08-24 18:57:50 +0200

    Add a totally unnecessary shortcut

--- engine/Buffer.cpp
+++ engine/Buffer.cpp
@@ -11,9 +11,13 @@ uint32_t Buffer::FindMemoryType(vk::raii::PhysicalDevice &physicalDevice, uint32
     for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; ++i)
     {
         bool memoryTypeBitsMatch = memoryTypeBits & (1 << i);
-        vk::MemoryPropertyFlags propertyFlagsAnded =
-            memoryProperties.memoryTypes[i].propertyFlags & memoryPropertyFlags;
-        if (memoryTypeBitsMatch && propertyFlagsAnded == memoryPropertyFlags)
+        if (!memoryTypeBitsMatch)
+        {
+            continue;
+        }
+        vk::MemoryPropertyFlags propertyFlags = memoryProperties.memoryTypes[i].propertyFlags;
+        vk::MemoryPropertyFlags propertyFlagsAnded = propertyFlags & memoryPropertyFlags;
+        if (propertyFlagsAnded == memoryPropertyFlags)
         {
             return i;
         }
