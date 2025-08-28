7102278efeb6f7433ca8133b90a4762f14226273
Dirk Zimmermann <me@dirkz.com>
2025-08-28 10:47:39 +0200

    VmaBuffer as smart pointer

--- engine/VmaBuffer.h
+++ engine/VmaBuffer.h
@@ -11,12 +11,29 @@ struct VmaBuffer
               vk::DeviceSize size)
         : m_allocator{allocator}, m_buffer{buffer}, m_allocation{allocation}, m_size{size} {};
 
+    VmaBuffer(std::nullptr_t) {};
+
     VmaBuffer() = delete;
     VmaBuffer(const VmaBuffer &) = delete;
 
+    VmaBuffer &operator=(VmaBuffer &&rhs) noexcept
+    {
+        if (this != &rhs)
+        {
+            std::swap(m_allocator, rhs.m_allocator);
+            std::swap(m_buffer, rhs.m_buffer);
+            std::swap(m_allocation, rhs.m_allocation);
+            std::swap(m_size, rhs.m_size);
+        }
+        return *this;
+    }
+
     ~VmaBuffer()
     {
-        vmaDestroyBuffer(m_allocator, m_buffer, m_allocation);
+        if (m_allocation)
+        {
+            vmaDestroyBuffer(m_allocator, m_buffer, m_allocation);
+        }
     };
 
     VmaBuffer &operator=(const VmaBuffer &) = delete;
@@ -29,10 +46,10 @@ struct VmaBuffer
     void CopyMemoryToAllocation(const void *src) const;
 
   private:
-    VmaAllocator m_allocator;
-    VkBuffer m_buffer;
-    VmaAllocation m_allocation;
-    vk::DeviceSize m_size;
+    VmaAllocator m_allocator = nullptr;
+    VkBuffer m_buffer = VK_NULL_HANDLE;
+    VmaAllocation m_allocation = nullptr;
+    vk::DeviceSize m_size = 0;
 };
 
 } // namespace vkdeck
