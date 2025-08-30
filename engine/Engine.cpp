86a9ba88e6350c4a083dde93e5a3548a8e66b9e1
Dirk Zimmermann <me@dirkz.com>
2025-08-30 12:23:20 +0200

    The vertex data's triangles are clockwise, so this is the correct rasterization state

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -480,8 +480,8 @@ void Engine::CreateGraphicsPipeline()
         depthClampEnable,
         rasterizerDiscardEnable,
         vk::PolygonMode::eFill,
-        vk::CullModeFlagBits::eFront,
-        vk::FrontFace::eCounterClockwise,
+        vk::CullModeFlagBits::eBack,
+        vk::FrontFace::eClockwise,
         depthBiasEnable,
         depthBiasConstantFactor,
         depthBiasClamp,
@@ -613,7 +613,7 @@ void Engine::UpdateUniformBuffer(FrameData &frameData)
     glm::mat4 view =
         glm::lookAt(glm::vec3{0.f, 0.f, 2.f}, glm::vec3{0.f, 0.f, 0.f}, glm::vec3{0.f, 0.f, 1.f});
     glm::mat4 projection = glm::perspective(glm::radians(45.f), ratio, 0.1f, 10.f);
-    UniformObject ubo{glm::mat4{1.f}, glm::mat4{1.f}, glm::mat4{1.f}};
+    UniformObject ubo{model, glm::mat4{1.f}, glm::mat4{1.f}};
 
     frameData.UniformBuffer().CopyMemoryToAllocation(&ubo);
 }
