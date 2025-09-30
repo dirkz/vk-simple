ad9ada0971c7d26fa30f633c44339e9d73a9e395
Dirk Zimmermann <me@dirkz.com>
2025-09-30 12:25:01 +0200

    Go 3D re vertex coordinates

--- engine/Vertex.h
+++ engine/Vertex.h
@@ -20,7 +20,7 @@ struct Vertex
         constexpr uint32_t location = 0;
 
         vk::VertexInputAttributeDescription descriptionPosition{
-            location, binding, vk::Format::eR32G32Sfloat, offsetof(Vertex, m_position)};
+            location, binding, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, m_position)};
 
         vk::VertexInputAttributeDescription descriptionColor{
             location + 1, binding, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, m_color)};
@@ -31,12 +31,12 @@ struct Vertex
         return {descriptionPosition, descriptionColor, descriptionTexCoordinate};
     }
 
-    Vertex(glm::vec2 position, glm::vec3 color, glm::vec2 texCoords)
+    Vertex(glm::vec3 position, glm::vec3 color, glm::vec2 texCoords)
         : m_position{position}, m_color{color}, m_texCoords{texCoords}
     {
     }
 
-    glm::vec2 &Position()
+    glm::vec3 &Position()
     {
         return m_position;
     }
@@ -52,7 +52,7 @@ struct Vertex
     }
 
   private:
-    glm::vec2 m_position;
+    glm::vec3 m_position;
     glm::vec3 m_color;
     glm::vec2 m_texCoords;
 };
